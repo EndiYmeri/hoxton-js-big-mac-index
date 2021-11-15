8ba457ff54050a7947de668490386d4a21b48ec8
EndiYmeri <56412395+EndiYmeri@users.noreply.github.com>
2021-11-15 17:20:49 +0100

    Challenge 2 completed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const USBigMacPriceUSD = 5.66 /* Price in USD*/
 const ItalianBigMacPriceEur = 6.80 /* Price in Euro*/
 const SwedenBigMacPriceSEK = 52.88 /* Price in Swedish Krona*/
-const RussianBigMacPriceRUB = 165 /* Price in Russian Ruble*/
+const RussianBigMacPriceRUB = 169 /* Price in Russian Ruble*/
 const SwissBigMacPriceCHF = 6.50 /* Price in Swiss Franc*/
 const CananadianBigMacPriceCAD = 6.77 /* Price in Canadian Dollar*/
 
@@ -11,7 +11,7 @@ const CananadianBigMacPriceCAD = 6.77 /* Price in Canadian Dollar*/
 
 const USDtoEUR = 0.88
 const USDtoSEK = 8.77
-const USDtoRUB = 72.73
+const USDtoRUB = 74.53
 const USDtoCHF = 0.92
 const USDtoCAD = 1.25
 
@@ -39,3 +39,23 @@ console.log("Sweden price index compard to US " + indexOfSweden.toFixed(2));
 console.log("Russia price index compard to US " + indexOfRussia.toFixed(2));
 console.log("Swiss price index compard to US " + indexOfSwiss.toFixed(2));
 console.log("Canada price index compard to US " + indexOfCanada.toFixed(2));
+
+
+// Challenge 2
+
+// OverValued
+
+indexOfItaly < 0 && console.log("The Italian Euro is: " + -indexOfItaly.toFixed(2) * 100 + "% overvalued")
+indexOfSweden < 0 && console.log("The Swedish Krona is: " + -indexOfSweden.toFixed(2) * 100 + "% overvalued")
+indexOfRussia < 0 && console.log("The Russian Ruble is: " + -indexOfRussia.toFixed(2) * 100 + "% overvalued")
+indexOfSwiss < 0 && console.log("The Swiss Franc is: " + -indexOfSwiss.toFixed(2) * 100 + "% overvalued")
+indexOfCanada < 0 && console.log("The Canadian Dollar is: " + -indexOfCanada.toFixed(2) * 100 + "% overvalued")
+
+
+
+// UnderValued
+indexOfItaly > 0 && console.log("The Italian Euro is: " + indexOfItaly.toFixed(2) * 100 + "% undervalued")
+indexOfSweden > 0 && console.log("The Swedish Krona is: " + indexOfSweden.toFixed(2) * 100 + "% undervalued")
+indexOfRussia > 0 && console.log("The Russian Ruble is: " + indexOfRussia.toFixed(2) * 100 + "% undervalued")
+indexOfSwiss > 0 && console.log("The Swiss Franc is: " + indexOfSwiss.toFixed(2) * 100 + "% undervalued")
+indexOfCanada > 0 && console.log("The Canadian Dollar is: " + indexOfCanada.toFixed(2) * 100 + "% undervalued")
