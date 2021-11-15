f48b2e7b51e8d4134394f8cc2a128c245d97b125
EndiYmeri <56412395+EndiYmeri@users.noreply.github.com>
2021-11-15 15:23:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// The Big Mac PPP exchange rate between two countries is obtained by dividing the price of a Big Mac in one country (in its currency) by the price of a Big Mac in another country (in its currency). This value is then compared with the actual exchange rate; if it is lower, then the first currency is under-valued (according to PPP theory) compared with the second, and conversely, if it is higher, then the first currency is over-valued.
+
+
+const USBigMacPrice = 5.66
+const ITBigMacPrice = 6.80
